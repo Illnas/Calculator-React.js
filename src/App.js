61e4c58caf618c5060cad39b67ca1caaf1a3dbef
Nadatari <dariosting@gmail.com>
2021-06-15 17:04:34 +0200

    Did styling and passed 7/16 fcc tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,91 @@
-
+import { useState } from "react"
 
 function App() {
+  const [numbers] = useState([
+    {
+      num: 1,
+      id: "one"
+    },
+    {
+      num: 2,
+      id: "two"
+    },
+    {
+      num: 3,
+      id: "three"
+    },
+    {
+      num: 4,
+      id: "four"
+    },
+    {
+      num: 5,
+      id: "five"
+    },
+    {
+      num: 6,
+      id: "six"
+    },
+    {
+      num: 7,
+      id: "seven"
+    },
+    {
+      num: 8,
+      id: "eight"
+    },
+    {
+      num: 9,
+      id: "nine"
+    },
+    {
+      num: ".",
+      id: "decimal"
+    },
+    {
+      num: 0,
+      id: "zero"
+    },
+    {
+      num: "=",
+      id: "equals"
+    },
+  ]);
+
+  const [operations] = useState([
+    {
+      op: "+",
+      id: "add"
+    },
+    {
+      op: "-",
+      id: "subtract"
+    },
+    {
+      op: "*",
+      id: "multiply"
+    },
+    {
+      op: "/",
+      id: "divide"
+    },
+    {
+      op: "Clear",
+      id: "clear"
+    },
+  ])
+
+
+
   return (
-    <div>
-     <h1>Hello</h1>
+    <div className="main">
+      <div id="display">First line</div>
+      <div id="display-second">Second line</div>
+      <div id="display-third">Result</div>
+
+      <div className="container">{numbers.map((e) => <button id={e.id}>{e.num}</button>)}</div>
+      <div className="operation-container">{operations.map((e) => <button  id={e.id}>{e.op}</button>)}</div>
+
     </div>
   );
 }
